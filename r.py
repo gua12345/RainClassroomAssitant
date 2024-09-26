9f249bd4d3a58855cc1a25ff54aa2e95910c794e
gua12345 <121714613+gua12345@users.noreply.github.com>
2024-09-26 14:16:47 +0800

    Update r.py

--- r.py
+++ r.py
@@ -1,6 +1,8 @@
 import sys
 from PyQt5 import QtWidgets, QtCore
 from UI.MainWindow import MainWindow_Ui
+from UI.MainWindow import MainWindow_Ui
+from Scripts.Send import send_group_message
 
 def close_app():
     QtWidgets.QApplication.quit()
@@ -14,11 +16,11 @@ if __name__ == "__main__":
     main.show()
     # 启动监听
     ui.active()
-    
-    # 设置定时器，100分钟后关闭应用程序
+    send_group_message("摸鱼助手监听启动成功") 
+    # 设置定时器，105分钟后关闭应用程序
     timer = QtCore.QTimer()
     timer.timeout.connect(close_app)
-    timer.start(100 * 60 * 1000)  # 100分钟转换为毫秒
-    
+    timer.start(105 * 60 * 1000)  # 100分钟转换为毫秒
+    send_group_message( "摸鱼助手启动成功")
     # 主窗体循环
     app.exec_()
